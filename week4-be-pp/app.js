8672c4b3f4c99b73dce2d9289246a2a56d46168e
farahelb <farah.elb26@gmail.com>
2024-09-09 15:04:49 +0300

    Implement Third-Party Middleware

--- week4-be-pp/app.js
+++ week4-be-pp/app.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const morgan = require('morgan');
 
 // Middleware to parse JSON
 app.use(express.json());
 
 
 // Use the tourRouter for all /tours routes
-app.use("/tours", tourRouter);
+app.use("/api/tours", tourRouter);
 
 // Use the userRouter for all /users routes
-app.use("/users", userRouter);
+app.use("/api/users", userRouter);
+
+app.use(morgan('tiny'));
 
 const port = 4000;
 // Start the server
